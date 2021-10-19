b66639773283b82d7ba9e8c1f3f821eadc22bcfd
jlew0007 <jlew0007@student.monash.edu>
2021-10-19 13:41:08 +1100

    updated code from moodle

--- submission/Player.hs
+++ submission/Player.hs
@@ -8,19 +8,46 @@ import           Cards              -- Finally, the generic card type(s)
 
 import           TwentyOne.Types    -- Here you will find types used in the game of TwentyOne
 import           TwentyOne.Rules    -- Rules of the game
-import Debug.Trace
+import           Debug.Trace
 
-ParseLastNumberOfLosses :: Maybe String -> Int
-ParseLastNumberOfLosses = 0
+-- take our memory string in which we encode as a Json object and return it in a usable form
+-- parseJsonMemoryString :: Maybe String -> [(String, JsonValue)]
+parseJsonMemoryString :: Maybe String -> [(String, Int)]
+parseJsonMemoryString _ = [(show "consecutiveLosses", 0)]
 
--- determine a good value for our bid
--- takes the min bid, max bid and 
-placeBid :: Maybe String -> (Action, String)
-placeBid _ = trace ("betting turn: " ++ show minBid) (Bid minBid, "")
+
+-- | take a maybe Int value (generally for a bid) and return an Int
+pointsToInt :: Maybe Points -> Int
+pointsToInt Nothing = 0
+pointsToInt (Just number) = number
+
+-- | take a maybe Int value (generally for a bid) and return an Int
+getIntOrZero :: Maybe Int -> Int
+getIntOrZero Nothing = 0
+getIntOrZero (Just number) = number
+
+-- employ the Martingale System - https://upswingpoker.com/the-best-blackjack-betting-strategy-basic-explanation/
+chooseBidValue :: Int -> Int -> Action
+chooseBidValue _ 0 = Bid minBid
+chooseBidValue points i = Bid $ foldr min points [(i * minBid), maxBid]
+
+-- lookup playerId
+lookupPlayerId :: [PlayerPoints] -> PlayerId -> Maybe Points
+lookupPlayerId [(a,b)] id = 
+
+-- filter points by ID
+-- get a given players remaining points
+getPointsById :: [PlayerPoints] -> PlayerId -> Int
+getPointsById player playerId = getIntOrZero $ lookup playerId (player)
+
+
+-- | place a bid given a number of losses
+placeBid :: Int -> [(String, Int)] -> (Action, String)
+placeBid points list = (chooseBidValue points $ getIntOrZero $ lookup "consecutiveLosses" list, "")
 
 -- | This function is called once it's your turn, and keeps getting called until your turn ends.
 playCard :: PlayFunc
 -- bidding turn at the start of a round.
-playCard Nothing _ _ _ memory _ = placeBid memory
+playCard Nothing allPoints _ thisPlayerId memory _ = placeBid ( getPointsById allPoints thisPlayerId) (parseJsonMemoryString memory)
 -- else play normally
 playCard (Just _) _ _ _ _ hand = trace ("play turn: cards " ++ show hand) (Bid 1, "")
