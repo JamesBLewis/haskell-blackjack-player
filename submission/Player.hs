4135459200c285c89790e25341db70a435f9705e
jlew0007 <jlew0007@student.monash.edu>
2021-10-22 12:01:46 +1100

    partially implimented table based gameplay

--- submission/Player.hs
+++ submission/Player.hs
@@ -10,6 +10,26 @@ import           TwentyOne.Types    -- Here you will find types used in the game
 import           TwentyOne.Rules    -- Rules of the game
 import           Debug.Trace
 
+-- | given a set of rules determine what to do when you see a given set of cards 
+attemptPairSplitting :: Hand -> Card -> Points -> Maybe Action
+attemptPairSplitting [Card _ Ace, Card _ Ace] _ points = Just $ Split points
+attemptPairSplitting [Card _ Ten, Card _ Ten] _ _ = Just Stand -- not actually sure about this...
+attemptPairSplitting [Card _ Nine, Card _ Nine] upCard points = case upCard of
+    Card _ Seven -> Just Stand -- not actually sure about this...
+    Card _ Ten -> Just Stand-- not actually sure about this...
+    Card _ Ace -> Just Stand-- not actually sure about this...
+    _ -> Just $ Split points
+attemptPairSplitting [Card _ Eight, Card _ Eight] _ points = Just $ Split points
+attemptPairSplitting [Card _ Seven, Card _ Seven] upCard points | upCard > 7 = Just $ Stand otherwise $ Split points
+attemptPairSplitting [Card _ Six, Card _ Six] upCard points | upCard > 6 = Just $ Stand otherwise $ Split points -- this is slightly wrong
+attemptPairSplitting [Card _ Five, Card _ Five] _ _ = Just Stand
+attemptPairSplitting [Card _ Four, Card _ Four] _ _ = Just Stand
+attemptPairSplitting [Card _ Three, Card _ Three] upCard points | upCard > 7 = Stand otherwise $ Split points -- this is slightly wrong
+attemptPairSplitting [Card _ Two, Card _ Two] upCard points | upCard > 7 = Stand otherwise $ Split points -- this is slightly wrong
+attemptPairSplitting _ _ _ = Nothing -- default case
+
+
+
 -- take our memory string in which we encode as a Json object and return it in a usable form
 -- parseJsonMemoryString :: Maybe String -> [(String, JsonValue)]
 parseJsonMemoryString :: Maybe String -> [(String, Int)]
@@ -31,23 +51,30 @@ chooseBidValue :: Int -> Int -> Action
 chooseBidValue _ 0 = Bid minBid
 chooseBidValue points i = Bid $ foldr min points [(i * minBid), maxBid]
 
--- lookup playerId
-lookupPlayerId :: [PlayerPoints] -> PlayerId -> Maybe Points
-lookupPlayerId [(a,b)] id = 
+pointsToTuple :: [PlayerPoints] -> [(PlayerId, Points)]
+pointsToTuple p = foldl (\a (PlayerPoints playerId points) -> (playerId, points):a) [] p
 
 -- filter points by ID
 -- get a given players remaining points
 getPointsById :: [PlayerPoints] -> PlayerId -> Int
-getPointsById player playerId = getIntOrZero $ lookup playerId (player)
-
+getPointsById points playerId = getIntOrZero $ lookup playerId $ pointsToTuple points
 
 -- | place a bid given a number of losses
 placeBid :: Int -> [(String, Int)] -> (Action, String)
 placeBid points list = (chooseBidValue points $ getIntOrZero $ lookup "consecutiveLosses" list, "")
 
+-- | cheat with a strategy table: https://www.blackjackapprenticeship.com/wp-content/uploads/2018/08/BJA_Basic_Strategy.jpg
+playHand :: Card -> Hand -> Action
+playHand upCard hand = Just $ head $ filter (not Nothing) $ [attemptPairSplitting, attemptPairSplitting, attemptPairSplitting, attemptPairSplitting] <*> [hand, upCard, minBid]
+--playHand upCard hand = attemptPairSplitting upCard hand (||||) (attemptSoftTotals upCard hand  (||||) (attemptHardTotals upCard hand (||||) attemptLateSurrender upCard hand))
+
 -- | This function is called once it's your turn, and keeps getting called until your turn ends.
 playCard :: PlayFunc
 -- bidding turn at the start of a round.
-playCard Nothing allPoints _ thisPlayerId memory _ = placeBid ( getPointsById allPoints thisPlayerId) (parseJsonMemoryString memory)
+playCard Nothing allPoints _ thisPlayerId memory _ = trace  "Bidding turn" $ placeBid ( getPointsById allPoints thisPlayerId) (parseJsonMemoryString memory)
 -- else play normally
-playCard (Just _) _ _ _ _ hand = trace ("play turn: cards " ++ show hand) (Bid 1, "")
+playCard (Just upCard) _ _ _ _ hand = trace ("play turn: cards " ++ show hand) (playHand upCard hand, "")
+
+
+
+
